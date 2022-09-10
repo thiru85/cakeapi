76325e9457b32a8892ee1a6f697c45e8f4184e60
Thirumalai Aiyalu <taiyalu@amazon.com>
2022-09-10 22:11:20 +0200

    pushing files and such

---
+++ app.py
@@ -0,0 +1,36 @@
+import sys
+import boto3
+import os
+import json
+import warnings
+import random
+from flask import Flask
+import socket
+from boto3.dynamodb.conditions import Key, Attr
+
+app = Flask(__name__)
+
+@app.route("/")
+def cake_api():
+
+    query_list=[]
+    for item in response['Items']:
+        query_list.append(item)
+    output=query_list[random.randint(0,len(response))]
+
+    return {
+        "id": output["cakeID"],
+        "cakeName": output["cakeName"],
+        "cakeSize": output["cakeSize"],
+        "containerID": socket.gethostname()
+    }
+
+
+warnings.filterwarnings('ignore', category=FutureWarning, module='botocore.client')
+
+dynamodbobj = boto3.resource("dynamodb")
+table = dynamodbobj.Table("cakeType")
+
+response = table.scan()
+
+app.run(host='0.0.0.0', port='80')
